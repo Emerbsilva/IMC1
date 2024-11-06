e207aa58f2b09f76f00e79d60b7ebc0d3fd3fb78
Emerbsilva <ebsindustrial@gmail.com>
2024-11-06 12:43:25 -0300

    realizado ajustes

--- app/src/main/java/com/ebs/imc/MainActivity.kt
+++ app/src/main/java/com/ebs/imc/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.ebs.imc
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.RequiresApi
 import com.ebs.imc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,38 +17,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val bt_calcular = binding.btCalcular
-        val mensagem = binding.mensagem
-
-        bt_calcular.setOnClickListener{
-
+        //
+        binding.btCalcular.setOnClickListener{
+            //
             val editPeso = binding.editPeso.text.toString()
             val editAltura = binding.editAltura.text.toString()
-
+            //
             if (editPeso.isEmpty()){
-                mensagem.setText("Informe o seu peso.")
+                binding.mensagem.setText("Informe o seu peso.")
             }else if (editAltura.isEmpty()){
-                mensagem.setText("Informe a sua altura.")
+                binding.mensagem.setText("Informe a sua altura.")
             }else{
                 CalcularIMC()
             }
-
         }
-
+        //
     }
     //
+    @RequiresApi(Build.VERSION_CODES.M)
+    @SuppressLint("ResourceAsColor")
     private fun CalcularIMC(){
         // captura novamente os valores dos campos e salva nas constantes
         val pesoID = binding.editPeso
         val alturaID = binding.editAltura
         //
-        val peso = Integer.parseInt(pesoID.text.toString())
+        val peso = java.lang.Float.parseFloat(pesoID.text.toString())
         val altura = java.lang.Float.parseFloat(alturaID.text.toString())
         //
         val resultado = binding.mensagem
         val imc = peso / (altura * altura)
-
+        //
         val Mensagem = when{
             imc <= 18.5 -> "Peso Baixo"
             imc <= 24.9 -> "Peso Normal"
@@ -54,35 +55,44 @@ class MainActivity : AppCompatActivity() {
             imc <= 39.9 -> "Obesidade (Grau 2)"
             else -> "Obesidade Mórbida (Grau 3)"
         }
-
+        //
         imc.toString()
-        resultado.setText("IMC:  $imc  \n$Mensagem")
+        resultado.setText(String.format("IMC:  %.2f  \n$Mensagem",imc))
+        if (imc <= 18.5){
+            resultado.setTextColor(getColor(R.color.yellow))
+        }else if (imc <= 24.9){
+            resultado.setTextColor(getColor(R.color.green))
+        }else if (imc <= 29.9){
+            resultado.setTextColor(getColor(R.color.orange))
+        }else{
+            resultado.setTextColor(getColor(R.color.red))
+        }
     }
     //
     // Implementar o menu na tela com o icone
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
+        //
         val inflate = menuInflater
         inflate.inflate(R.menu.menu_principal,menu)
-
+        //
         return true
     }
-
+    //
     // Imlementar a ação do icone do menu
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
+        //
         when (item.itemId){
             R.id.reset ->{
                 val limparEditPeso = binding.editPeso
                 val limparEditAltura = binding.editAltura
                 val limparMensagem = binding.mensagem
-
+                //
                 limparEditPeso.setText("")
                 limparEditAltura.setText("")
                 limparMensagem.setText("")
             }
         }
-
+        //
         return super.onOptionsItemSelected(item)
     }
 }
