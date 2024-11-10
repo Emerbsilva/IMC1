ecec8ae53c918c592a45c0c712a090ed5d71c6d0
Emerbsilva <ebsindustrial@gmail.com>
2024-11-10 18:30:13 -0300

    usando conversão diferente

--- app/src/main/java/com/ebs/imc/MainActivity.kt
+++ app/src/main/java/com/ebs/imc/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -34,15 +35,13 @@ class MainActivity : AppCompatActivity() {
         //
     }
     //
-    @RequiresApi(Build.VERSION_CODES.M)
-    @SuppressLint("ResourceAsColor")
     private fun CalcularIMC(){
         // captura novamente os valores dos campos e salva nas constantes
-        val pesoID = binding.editPeso
-        val alturaID = binding.editAltura
+        val peso = binding.editPeso.text.toString().toFloat()
+        val altura = binding.editAltura.text.toString().toFloat()
         //
-        val peso = java.lang.Float.parseFloat(pesoID.text.toString())
-        val altura = java.lang.Float.parseFloat(alturaID.text.toString())
+        //val peso = java.lang.Float.parseFloat(pesoID.text.toString())
+        //val altura = java.lang.Float.parseFloat(alturaID.text.toString())
         //
         val resultado = binding.mensagem
         val imc = peso / (altura * altura)
